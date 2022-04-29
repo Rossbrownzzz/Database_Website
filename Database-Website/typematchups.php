5b82de3fa08bd706a2555d3c972470c49eba66fa
rossbrownzzz <ross-brown@uiowa.edu>
2022-04-28 22:30:03 -0500

    type matchu basic query stuff. needs huge overhaul

--- Database-Website/typematchups.php
+++ Database-Website/typematchups.php
@@ -1,4 +1,19 @@
 <!DOCTYPE html>
+<style>
+td {
+border: 1px solid black;
+}
+
+table tr:nth-child(odd) {
+    background-color: #ccc;
+}
+table {
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
+
+
 <html lang="en">
 	<head>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
@@ -39,6 +54,97 @@
                 </ul>
             </div>
 		</nav>
-		<h2>Type Matchups</h2>
+		<main>
+					<h1>
+					<?php
+
+
+					//TODO fix the formatting of the search bar, it looks terrible, and also line "echo $userQuery;" looks terrible too.
+					echo
+						'<form>
+						<label for="queryVal">Search:</label>
+						<input type="text" id="queryVal" name="queryVal"><br>
+						</form>'; 
+
+					//TODO add some kind of help popup or page thing that has some basic rules of how to search
+
+					//TODO allow searching - only by name or egg type for this page.
+					//regex validates input
+
+                    $allowable = 
+"/.*/";
+					//if a query is there
+					if (isset($_GET['queryVal']) && "" != $_GET['queryVal']){
+						// and it is allowable
+						if (preg_match($allowable, $_GET['queryVal'])){
+							$userQuery = $_GET['queryVal'];
+							// display the current query conditions
+							echo $userQuery;
+							$query = "SELECT pokedex_number, pokemon.name, egg_cycles, percentage_male, egg_pokemonType FROM pokemon JOIN eggpokemontype2 ON eggpokemontype2.name = pokemon.name;";
+						}
+						else{
+							echo "invalid query";
+							//default if invalid
+							$query = "SELECT pokedex_number, pokemon.name, egg_cycles, percentage_male, egg_pokemonType FROM pokemon JOIN eggpokemontype2 ON eggpokemontype2.name = pokemon.name;";
+						}
+					}
+					else{
+						//default
+						$query = "SELECT pokedex_number, pokemon.name, egg_cycles, percentage_male, egg_pokemonType FROM pokemon JOIN eggpokemontype2 ON eggpokemontype2.name = pokemon.name;";
+					}
+					
+
+					// format as table
+					echo "<div><table>";
+
+					displayData($query);
+
+
+					//TODO organize the table so it just fills the screen and nothing more
+					function displayData($sqlquery){
+						//establish connection
+						$servername = "localhost";
+						$username = "admin";
+						$password = "workplaceready";
+						$dbname = "pokemondb";
+						$conn = new mysqli($servername, $username, $password, $dbname);
+						
+						if ($conn->connect_error) {
+							die("Connection failed: " . $conn->connect_error);
+							}
+
+						//query the bale
+						$result = $conn->query($sqlquery);
+
+						//display all headers
+						echo "<tr>";
+						//name
+						echo "<td align='center' style='font-size:25px'>pokedex #</td>";
+						echo "<td align='center' style='font-size:25px'>name</td>";
+						echo "<td align='center' style='font-size:25px'>percent <br> male</td>";
+						echo "<td align='center' style='font-size:25px'>egg <br> cycles</td>";
+						echo "<td align='center' style='font-size:25px'>egg <br> type</td>";
+		
+
+						echo "</tr>\n";
+
+
+						//display all values
+						while($row = $result->fetch_assoc()) {
+							echo "<tr>";
+							echo "<td align='center' style='font-size:25px'>$row[pokedex_number]</td>";
+							echo "<td align='center' style='font-size:25px'>$row[name]</td>";
+							echo "<td align='center' style='font-size:25px'>$row[percentage_male]</td>";
+							echo "<td align='center' style='font-size:25px'>$row[egg_cycles]</td>";
+							echo "<td align='center' style='font-size:25px'>$row[egg_pokemonType]</td>";
+							echo "</tr>\n";
+							}
+						echo "</table></div>";
+						$conn->close();
+					}
+
+					?>
+					</h1>
+				</main>
 	</body>
 </html>
